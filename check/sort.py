9f541a1271d502b30fa6369c49d3e7f4dfcc4706
sathish48-codoid <54182296+sathish48-codoid@users.noreply.github.com>
2019-11-25 16:14:58 +0530

    Add files via upload

---
+++ check/sort.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+def month():
+    tourmonth = ['ddummy', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                 'November', 'December'];
+    currentMonth = datetime.now().month
+    # currentMonth = 7
+    data = currentMonth;
+    list = [];
+    i = 0;
+    j = 0;
+    for i in range(12):
+        if i > 0:
+            if (currentMonth >= 12):
+                if (j > data):
+                    print("false")
+                else:
+                    list.append(tourmonth[j + 1]);
+                    j += 1
+            else:
+                list.append(tourmonth[currentMonth + 1])
+                currentMonth += 1
+        else:
+            list.append(tourmonth[currentMonth])
+        i += 1
+
+print(list)
